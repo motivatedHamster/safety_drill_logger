ad1c1836420ff6ad95b7e36c46f45224d9358bf2
john smith <puzzle@gmail.com>
2015-05-19 00:02:25 +0000

    datagrid seems to be working

---
+++ app/grids/drills_grid.rb
@@ -0,0 +1,17 @@
+class DrillsGrid
+
+  include Datagrid
+
+  scope do
+    Drill
+  end
+
+  filter(:id, :integer)
+  filter(:created_at, :date, :range => true)
+
+  column(:id)
+  column(:name)
+  column(:created_at) do |model|
+    model.created_at.to_date
+  end
+end
