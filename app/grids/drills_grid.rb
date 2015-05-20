9c2bec8331c680c343e6c61f83a307969a7017f6
john smith <puzzle@gmail.com>
2015-05-20 17:40:12 +0000

    datagrid scaffold worked, needs lots of customization

--- app/grids/drills_grid.rb
+++ app/grids/drills_grid.rb
@@ -10,7 +10,7 @@ class DrillsGrid
   filter(:created_at, :date, :range => true)
 
   column(:id)
-  column(:name)
+  # column(:name)
   column(:created_at) do |model|
     model.created_at.to_date
   end
