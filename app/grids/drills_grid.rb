d9ccacc0221afcb109defecd6d385fac073be2d7
motivatedHamster <fangrock@live.com>
2015-05-22 23:25:00 +0000

    some columns added to drills table via inner joins

--- app/grids/drills_grid.rb
+++ app/grids/drills_grid.rb
@@ -5,6 +5,11 @@ class DrillsGrid
   scope do
     Drill
   end
+  
+#  scope do
+#    Drill.select(
+#      "drills.date_performed, classrooms.name as classroom_name")
+#  end
 
   filter(:id, :integer)
   filter(:created_at, :date, :range => true)
@@ -14,4 +19,26 @@ class DrillsGrid
   column(:created_at) do |model|
     model.created_at.to_date
   end
-end
+  # :date_performed
+  column(:date_performed)
+  # class name
+  column(:classroom) do |some_classroom|
+    some_classroom.name
+  end
+  # teacher name
+#  column(:teacher) do |some_teacher|
+#    some_teacher.name
+#  end
+  # teacher email
+#  column(:teacher) do |some_teacher|
+#    some_teacher.email
+#  end
+  # drill_type name
+#  column(:drill_type) do |some_drill_type|
+#    some_drill_type.name
+#  end
+  # drill_type required frequency
+#  column(:drill_type) do |some_drill_type|
+#    some_drill_type.required_frequency
+#  end
+end  # class DrillsGrid
