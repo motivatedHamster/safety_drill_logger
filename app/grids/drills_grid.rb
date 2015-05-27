bd3691025aa8268d0e9097fd027a0459df205759
motivatedHamster <fangrock@live.com>
2015-05-27 19:33:46 +0000

    some cleanup

--- app/grids/drills_grid.rb
+++ app/grids/drills_grid.rb
@@ -2,50 +2,55 @@ class DrillsGrid
 
   include Datagrid
 
-#  scope do
-#    Drill
-#  end
-  
   scope do
     Drill.select(
       "drills.date_performed, drills.created_at, drills.id, classrooms.name as classroom_name, teachers.name as teacher_name, teachers.email as teacher_email, drill_types.name as drill_name, drill_types.required_frequency as drill_frequency" ).joins(:classroom, :teacher, :drill_type)
   end
 
-  #filter(:id, :integer)
   filter(:created_at, :date, :range => true)
   filter(:date_performed, :date, :range => true)
-#  filter(:classroom_name)
   filter(:classroom_id, :enum,
-    :select => lambda {Classroom.all.map {|c| [c.name, c.id]}},
+    :select => lambda { Classroom.all.map {|c| [c.name, c.id]} },
     :multiple => true,
     :include_blank => false
   ) do |value|
     self.where(:drills => {:classroom_id => value})
   end
-#  filter(:teacher_name)
-#  filter(:drill_name)
+  filter(:teacher_id, :enum,
+    :select => lambda { Teacher.all.map {|t| [t.name, t.id]} },
+    :multiple => true,
+    :include_blank => false
+  ) do |value|
+    self.where(:drills => {:teacher_id => value})
+  end
+
+  #  trying to group multiple drill types based on common name
+#  dtype_names = DrillType.all.map {|d| [d.name, d.id]}
+#  uniq_dtype_names = dtype_names.uniq {|d| d[:name]}
+#  filter(:drill_type_id, :enum,
+#    :select => uniq_dtype_names,
+#    :multiple => true,
+#    :include_blank => false
+#  ) do |value|
+#    self.where(:drills => {:drill_type_id => value})
+#  end
+
+  filter(:drill_type_id, :enum,
+    :select => lambda { DrillType.all.map {|d| [d.name, d.id]} },
+    :multiple => true,
+    :include_blank => false
+  ) do |value|
+    self.where(:drills => {:drill_type_id => value})
+  end
 
   column(:id)
-  # column(:name)
   column(:created_at) do |model|
     model.created_at.to_date
   end
-  # :date_performed
   column(:date_performed)
-  # class name
   column(:classroom_name)
-  # teacher name
   column(:teacher_name)
-  # teacher email
   column(:teacher_email)
-  # drill_type name
-#  column(:drill_type) do |some_drill_type|
-#    some_drill_type.name
-#  end
   column(:drill_name)
-  # drill_type required frequency
-#  column(:drill_type) do |some_drill_type|
-#    some_drill_type.required_frequency
-#  end
   column(:drill_frequency)
 end  # class DrillsGrid
