998683f2b1cee3ad7baa76bc47266cb4848aec8c
motivatedHamster <fangrock@live.com>
2015-05-28 06:54:00 +0000

    minor

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,5 +39,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  config.action_mailer.default_url_options = { host: 0.0.0.0, port: 3000 }
 end
