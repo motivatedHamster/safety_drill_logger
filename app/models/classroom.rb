f97308a9705f76d7a8f9640be30bbf8f32d4e94e
John Smith <puzzle@gmail.com>
2015-08-29 11:29:23 -0700

    some work on crud cleanup

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -5,15 +5,17 @@ class Classroom < ActiveRecord::Base
   # ex: one classroom only has to perform fire drills quarterly while the other classrooms have to perform them monthly
   has_many :drill_types, dependent: :destroy
   belongs_to :teacher
+  accepts_nested_attributes_for :drill_types
+  accepts_nested_attributes_for :teachers
 
   def needs_drill?(drill_type)
     # return true if drill of drill_type has not occurred within current frequency
   end
-  
+
   def deadline?(drill_type)
     # returns number of days left in current frequency if needs_drill?(drill_type) = true, otherwise returns nil
   end
-  
+
   def conduct_drill(teacher, drill_type, date_conducted)
     # creates a new drill for this classroom & adds it to the database
   end
