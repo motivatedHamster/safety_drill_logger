3dba3ea0f28ef5986918128b5292cf2e55509cef
John Smith <puzzle@gmail.com>
2015-08-31 11:44:59 -0700

    attempting to add blank form field for drill_types to classroom edit

--- app/helpers/classrooms_helper.rb
+++ app/helpers/classrooms_helper.rb
@@ -1,2 +1,9 @@
 module ClassroomsHelper
+#  def setup_classroom(classroom)
+#    if classroom.drill_types.count == 0
+#      classroom.drill_types = [ DrillType.new, DrillType.new ]
+#    else
+#      classroom.drill_types = classroom.drill_types + [ DrillType.new ]
+#    end
+#  end
 end
